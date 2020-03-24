8ac8172b962f0d4e874eac4125c80078de4fcef2
jaymagrob <johnmagnusrobertson@gmail.com>
2020-03-24 17:01:44 +0000

    Content now added

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 function init() {
-  const outerBox = document.querySelectorAll('.outerBox')
-  console.log(outerBox)
 
-  var isScrolling
-
-  window.addEventListener('scroll', () => {
-  
-    window.clearTimeout( isScrolling )
-  
-    isScrolling = setTimeout(function() {
-    
-      console.log('SCROLL Y',window.scrollY)
-      console.log('INNER HEIGHT',window.innerHeight)
-      console.log('TOTAL BODY', document.body.scrollHeight)
-
-    }, 66)
-
-  }, false)
 
 }
 
