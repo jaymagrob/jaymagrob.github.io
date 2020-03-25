840bc76e9c4ba1e91f3cfe7441e3b280037386f4
jaymagrob <johnmagnusrobertson@gmail.com>
2020-03-25 17:17:55 +0000

    Hero page done. Move onto nav

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 function init() {
+  //DOM elements
+  const domStartButton = document.querySelector('.start-button')
+  
 
-
+  //event listeners
+  domStartButton.addEventListener('click',() => {
+    document.location.href = '#bio'
+  })
 }
 
 window.addEventListener('DOMContentLoaded', init)
