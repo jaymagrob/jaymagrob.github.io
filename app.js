67b0f576ad2e44178f0b3771ac1a7bd11b9dcca4
jaymagrob <johnmagnusrobertson@gmail.com>
2020-03-26 11:12:24 +0000

    background and parrallax undated

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 function init() {
   //DOM elements
   const domStartButton = document.querySelector('.start-button')
-  
+  const domHeroSize = document.querySelector('#hero-body')
+  const domNav = document.querySelector('nav')
+  const domNavLists = document.querySelectorAll('nav ul li')
 
   //event listeners
+  domNavLists.forEach(i => {
+    i.addEventListener('click', () => {
+      domNav.classList.add('hidden')
+    })
+  })
+
   domStartButton.addEventListener('click',() => {
     document.location.href = '#bio'
+  })
+
+  window.addEventListener('scroll',(e) => {
+    if (window.scrollY >= domHeroSize.scrollHeight) {
+      domNav.classList.remove('hidden')
+    } else {
+      domNav.classList.add('hidden')
+    }
   })
 }
 
