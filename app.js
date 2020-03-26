efddb72de8a7b08f78d012fdcf1e930dd6ed467f
jaymagrob <johnmagnusrobertson@gmail.com>
2020-03-26 16:51:22 +0000

    awesome fonts removed

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 function init() {
   //DOM elements
   const domStartButton = document.querySelector('.start-button')
-  const domHeroSize = document.querySelector('#hero-body')
-  const domNav = document.querySelector('nav')
   const domNavLists = document.querySelectorAll('nav ul li')
   const domInput = document.querySelector('input')
 
@@ -16,14 +14,6 @@ function init() {
   domStartButton.addEventListener('click',() => {
     document.location.href = '#bio'
   })
-
-  // window.addEventListener('scroll',(e) => {
-  //   if (window.scrollY >= domHeroSize.scrollHeight) {
-  //     domNav.classList.remove('hidden')
-  //   } else {
-  //     domNav.classList.add('hidden')
-  //   }
-  // })
 }
 
 window.addEventListener('DOMContentLoaded', init)
